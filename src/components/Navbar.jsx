d11e64bd75cee9b61b60f6e55c4eb90c1b8fbf6e
leandro <leaguts21@gmail.com>
2022-04-06 23:36:35 -0500

    funciona con morado

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './styles/navbar.css'
+
+
+const Navbar = () => {
+  return (
+    <div className='container'>
+        <span>Leandro</span>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/components/Sectionone.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './styles/sectionone.css'
+
+const Sectionone = () => {
+  return (
+    <div className='container2'>
+         <div class="wrapper">
+            <div class="neon-wrapper">
+                <span class="txt">&#123;L&#125;</span>
+                <span class="gradient"></span>
+                <span class="dodge"></span>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Sectionone
