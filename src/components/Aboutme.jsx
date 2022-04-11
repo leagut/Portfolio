16a458a0f5001e1e57259529fd8c7a600c6b76a0
leandro <leaguts21@gmail.com>
2022-04-10 21:11:25 -0500

    iconos

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './styles/aboutme.css'
-import im from '../images/ventana.svg'
+import im from '../images/ventana.JPG'
 
 const Aboutme = () => {
   
--- src/components/Tecnologies.jsx
+++ src/components/Tecnologies.jsx
@@ -1,36 +1,36 @@
 import React from 'react'
 import html from './styles/tecnologies.css'
 
-import bootstrap from'../images/im/bootstrap-logo.png'
-import figma  from'../images/im/figma.png'
-import git from'../images/im/git.png'
-import github from'../images/im/github.png'
-import html2 from'../images/im/Html5_diseño_web-1.png'
-import js from'../images/im/js.png'
-import css from'../images/im/logo-2582747_1280.webp'
-import react from'../images/im/medal-react.png'
-import mongo from'../images/im/mongodb.png'
-import mysql from'../images/im/msq2.png'
-import node from'../images/im/node.jpg'
-import taild  from '../images/im/Tailwind-img.png'
-import ux from'../images/im/uiux-img1.png'
+import {AiOutlineHtml5} from 'react-icons/ai'
+import {FaCss3Alt,FaReact} from 'react-icons/fa'
+import {SiJavascript,SiMongodb,SiFigma,SiTailwindcss} from 'react-icons/si'
+import {BsGithub} from 'react-icons/bs'
+import {DiGit} from 'react-icons/di'
+import {GrMysql} from 'react-icons/gr'
+import {BsBootstrapFill} from 'react-icons/bs'
+
+
+
+
+
+
+
 
 const Tecnologies = () => {
   return (
     <div className='container-tecnologies'>
-      <img src={bootstrap} alt="" className='size' />
-      <img src={figma} alt="" className='size' />
-      <img src={git} alt=""  className='size'/>
-      <img src={github} alt=""  className='size'/>
-      <img src={html2} alt="" className='size' />
-      <img src={js} alt=""  className='size'/>
-      <img src={css} alt="" className='size' />
-      <img src={react} alt="" className='size' />
-      <img src={mongo} alt="" className='size' />
-      <img src={mysql} alt=""  className='size'/>
-      <img src={node} alt="" className='size' />
-      <img src={taild} alt=""  className='size'/>
-      <img src={ux} alt=""  className='size'/>
+      <AiOutlineHtml5 className='iconn    size'/>
+      <FaCss3Alt className='iconn   size'/>
+      <SiJavascript className='iconn    size'/>
+      <FaReact className='iconn   size'/>
+      <SiMongodb className='iconn   size'/>
+      <SiFigma className='iconn   size'/>
+      <BsGithub className='iconn    size'/>
+      <DiGit className='iconn   size'/>
+      <GrMysql className='iconn   size'/>
+      <SiTailwindcss className='iconn   size'/>
+      <BsBootstrapFill className='iconn   size'/>
+      
     </div>
   )
 }
