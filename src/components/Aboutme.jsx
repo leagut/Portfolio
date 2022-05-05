f09542ab7668eb63fc01326925e704162251e76a
leandro <leaguts21@gmail.com>
2022-05-05 16:22:53 -0500

    acomodando5

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -10,8 +10,8 @@ const Aboutme = () => {
 
       
       <div className='text'>
-      ¡Hola!, Soy Leandro, me considero una persona muy curiosa y creativa. Mi ambiente favorito de desarrollo es el MERN, mi objetivo primordial es crear soluciones que generen una plusvalía donde trabaje y hacia sus clientes. Cuento con habilidades en metodología scrum y gestión de proyectos junto con un pensamiento creativo y catalizador de ideas con el cual puedo enriquecer los grupos donde trabajo.
-      
+      ¡Hola! , Soy Leandro, me considero una persona muy curiosa y creativa. Mi ambiente favorito de desarrollo es el MERN, mi objetivo primordial es crear soluciones que generen una plusvalía donde trabajo y hacía sus clientes. Cuento con habilidades en metodología scrum y gestión de proyectos junto con un pensamiento creativo y catalizador de ideas con el cual puedo enriquecer los grupos donde trabajo.
+
       </div>
 
     
