38ed586e6659bc9b209dc878f350086bc171e3d5
leandro <leaguts21@gmail.com>
2022-05-05 20:48:24 -0500

    solo faltan projects

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -10,7 +10,7 @@ const Aboutme = () => {
 
       
       <div className='text'>
-      ¡Hola! , Soy Leandro, me considero una persona muy curiosa y creativa. Mi ambiente favorito de desarrollo es el MERN, mi objetivo primordial es crear soluciones que generen una plusvalía donde trabajo y hacía sus clientes. Cuento con habilidades en metodología scrum y gestión de proyectos junto con un pensamiento creativo y catalizador de ideas con el cual puedo enriquecer los grupos donde trabajo.
+      ¡Hola!  Soy Leo, me considero una persona muy curiosa y creativa. Mi ambiente favorito de desarrollo es el MERN, mi objetivo primordial es crear soluciones que generen una plusvalía donde trabajo y hacía sus clientes. Cuento con habilidades en metodología scrum y gestión de proyectos junto con un pensamiento creativo y catalizador de ideas con el cual puedo enriquecer los grupos donde trabajo.
 
       </div>
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 import './styles/footer.css'
 const Footer = () => {
   return (
-    <div className='footer-container'>
+
+    <div className='lastco'>
+      <div className='footer-container'></div>
+
+
       
+
+
+            <div class="wrapper2">
+                <div class="neon-wrapper2">
+                    <span class="txt2">Creating  with Love</span>
+                    <span class="gradient2"></span>
+                    <span class="dodge2"></span>
+                </div>
+            </div>
     </div>
+    
   )
 }
 
